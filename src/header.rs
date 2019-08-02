a6b2c5c47acfc7e4cb6ec98552443f9b6fa021de
MarimeGui <lepro.guillaume@gmail.com>
2019-08-02 18:10:23 +0200

    Implemented TimeScale

--- src/header.rs
+++ src/header.rs
@@ -4,10 +4,16 @@ use ez_io::{ReadE, WriteE};
 use std::io::{Read, Write};
 
 /// Contains the information found in a standard 6-byte MThd Header of a MIDI File.
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub struct SMFHeader {
+    /// Header Length
+    pub length: u32,
     /// Format of this MIDI File
-    format: Format,
+    pub format: Format,
+    /// Number of tracks after the header
+    pub nb_tracks: u16,
+    /// Provides information on what the delta times represent
+    pub time_divisions: TimeScale,
 }
 
 impl SMFHeader {
@@ -23,7 +29,7 @@ impl SMFHeader {
 }
 
 /// The Format field in a MIDI File Header.
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub enum Format {
     /// Single Track in the MIDI File.
     SingleTrack,
@@ -56,6 +62,36 @@ impl Format {
     /// Writes the u16 Format field in the header
     pub fn export<W: Write>(&self, writer: &mut W) -> Result<()> {
         writer.write_be_to_u16(self.get_value())?;
+        Ok(())
+    }
+}
+
+/// The possible time scales a MIDI File can use.
+#[derive(Copy, Clone)]
+pub enum TimeScale {
+    TicksPerQuarterNote(u16),
+    SMPTECompatible(i8, u8)
+}
+
+impl TimeScale {
+    /// Reads the 2-byte Division Information inside of MThd
+    pub fn import<R: Read>(reader: &mut R) -> Result<TimeScale> {
+        let data = reader.read_be_to_u16()?;
+        Ok(match (data & 0b1000_0000_0000_0000u16) == 1 {
+            true => TimeScale::TicksPerQuarterNote(data),
+            false => TimeScale::SMPTECompatible(((data & 0b1111_1111_0000_0000) >> 8) as i8, (data & 0b0000_0000_1111_1111) as u8), // Unsure if the "as i8" here will work properly...
+        })
+    }
+
+    /// Writes the Timing Information inside MThd
+    pub fn export<W: Write>(self, writer: &mut W) -> Result<()> {
+        match self {
+            TimeScale::TicksPerQuarterNote(v) => writer.write_be_to_u16(v)?,
+            TimeScale::SMPTECompatible(v, w) => {
+                writer.write_to_i8(v)?;
+                writer.write_to_u8(w)?;
+            }
+        }
         Ok(())
     }
 }
